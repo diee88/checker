d9a6bfebd14a90af1818cc89365ae98e86bb1e0d
Endi Desyuntiadi <endi.desyuntiadi@gmail.com>
2020-07-22 02:08:43 +0700

    new key merchant

--- checker/src/main/java/com/spe/checker/SpeChecker.java
+++ checker/src/main/java/com/spe/checker/SpeChecker.java
@@ -55,7 +55,8 @@ public class SpeChecker {
             hasil = true;
         } else if (signature.compareTo("8zpZUzVcOows43YzN8plKTRZ8Ow=\n") != 0) != 0) {
             hasil = true;*/
-        } else if (packageName.compareTo("id.co.bni.yapcustomer") != 0) {
+//        } else if (packageName.compareTo("id.co.bni.yapcustomer") != 0) {
+        } else if (packageName.compareTo("id.co.bni.yapmerchant") != 0) {
             hasil = true;
         } else if (signature.compareTo("AAzC5u4pYW+Z2kZhaCXG/E8/KBk=\n") != 0) {
             hasil = true;
